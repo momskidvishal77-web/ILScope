e71c698deaea1f5588dd957042e46daa77d9d6b0
momskidvishal77-web <momskidvishal77@gmail.com>
2026-02-01 16:31:25 +0530

    Initial commit: setup ILScope with assembly loader

---
+++ ILScope/Core/AssemblyLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+namespace ILScope.Core
+{
+    internal class AssemblyLoader
+    {
+        public static Assembly LoadAssembly(string assemblyPath)
+        {
+            if (!File.Exists(assemblyPath))
+            {
+                throw new FileNotFoundException("Assembly file not found", assemblyPath);
+            }
+            assemblyPath = Path.GetFullPath(assemblyPath);
+
+            return AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
+        }
+    }
+}
---
+++ ILScope/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using ILScope.Core;
+
+namespace ILScope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ILScope - .NET Intermediate Language Disassembler");
+            Console.WriteLine("==============================================");
+
+            Console.Write("Enter path to .NET assembly (.dll) : ");
+            string? path = Console.ReadLine();
+
+            try
+            {
+                Assembly assembly = AssemblyLoader.LoadAssembly(path!);
+                Console.WriteLine("\n Assembly Loaded Successfully!");
+                Console.WriteLine($"Name  : {assembly.FullName}");
+                Console.WriteLine($"Location: {assembly.Location}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
